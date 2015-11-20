2d0d502edd745ff076f6f712b6c32146f1b92342
practicum <practicum@wcw-stud-145-109-1-179.wireless.uva.nl>
2015-11-20 12:52:26 +0100

    version 2.0

--- To-Do/To-Do/ViewController.swift
+++ To-Do/To-Do/ViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var listsData:[String]!
+    
+    // Outlets.
+    @IBOutlet weak var listsOutlet: UITableView!
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        listsData = []
+        listsOutlet.delegate = self
+        listsOutlet.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +28,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Button action.
+    @IBAction func addButton(sender: AnyObject) {
+        //add your data into tables array from textField
+        listsData.append(textFieldOutlet.text!)
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            //reload your tableView
+            self.listsOutlet.reloadData()
+        })
+        
+        textFieldOutlet.text = ""
+        print(listsData)
+    }
+    
+    // Delegate methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listsData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if tableView == listsOutlet {
+            let cell = listsOutlet.dequeueReusableCellWithIdentifier("TextCell", forIndexPath: indexPath)
+            
+            let row = indexPath.row
+            cell.textLabel?.text = listsData[row]
+        
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(listsData[row])
+    }
 }
 
